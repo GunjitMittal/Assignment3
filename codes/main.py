1dce4e3de8a81f51cf1aef00ce70b8a95f8b4bed
GunjitMittal <101900401+GunjitMittal@users.noreply.github.com>
2022-04-19 16:47:43 +0530

    Add files via upload

--- codes/main.py
+++ codes/main.py
@@ -1,23 +1,13 @@
-from tabulate import tabulate
 import numpy as np
 import pandas as pd
-read = pd.read_excel(r'question.xlsx')
+read = pd.read_excel("question.xlsx","Sheet1")
 data = np.array(read)
-l=len(data)
-k=len(data[0])
-a0=0;a1=0;a2=0;a3=0
-for i in range(l):
-    for j in range(k):
-        if (data[i][j]==0):
-            a0+=1
-        if (data[i][j]==1):
-            a1+=1
-        if (data[i][j]==2):
-            a2+=1
-        if (data[i][j]==3):
-            a3+=1
-dat = [[0,a0],
-[1,  a1],
-[2, a2],
-[3, a3]]
-print(tabulate(dat, headers=["No. of heads", "Frequency"]))
+values = list(data[0])+list(data[1])+list(data[2])
+a0 = values.count(0)
+a1 = values.count(1)
+a2 = values.count(2)
+a3 = values.count(3)
+no_of_heads = [0,1,2,3]
+frequencies = [a0,a1,a2,a3]
+write = pd.DataFrame({"No. of heads":no_of_heads,"Frequency":frequencies})
+write.to_excel("output.xlsx",index=False)
