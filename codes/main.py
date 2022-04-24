9c886734b6fba4fe6ec0ec0ce3e1d21746fd319d
GunjitMittal <101900401+GunjitMittal@users.noreply.github.com>
2022-04-24 11:40:18 +0530

    Add files via upload

--- codes/main.py
+++ codes/main.py
@@ -2,12 +2,8 @@ import numpy as np
 import pandas as pd
 read = pd.read_excel("question.xlsx","Sheet1")
 data = np.array(read)
-values = list(data[0])+list(data[1])+list(data[2])
-a0 = values.count(0)
-a1 = values.count(1)
-a2 = values.count(2)
-a3 = values.count(3)
-no_of_heads = [0,1,2,3]
-frequencies = [a0,a1,a2,a3]
-write = pd.DataFrame({"No. of heads":no_of_heads,"Frequency":frequencies})
+data=data.ravel()
+data=pd.Series(data)
+freq=data.value_counts(sort=False)
+write = pd.DataFrame({"No. of heads":[i for i in range(4)],"Frequency":freq})
 write.to_excel("output.xlsx",index=False)
